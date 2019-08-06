21f052fb48325ad0320c96c03b18af372b88ead5
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-06 14:38:14 -0500

    Changed to proper api link

--- github-user-card/src/components/Data.js
+++ github-user-card/src/components/Data.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 import User from './User.js'
 
 export default class Data extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             userData: ""
         };
     }
 
     componentDidMount() {
-        const userData = axios.get("https://github.com/users/mariam-farrukh")
+        const userData = axios.get("https://api.github.com/users/mariam-farrukh", {})
         .then(res => {
             this.setState({userData: res.data});
         })
